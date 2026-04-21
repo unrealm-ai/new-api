e81739cf134286893da1f59c787f139fec23f2b9
LarryLian <lianjunwen@lianjunwendeMacBook-Pro.local>
2026-04-21 22:44:34 +0800

    日志审计增加开关

--- common/constants.go
+++ common/constants.go
@@ -73,6 +73,7 @@ var DebugEnabled bool
 var MemoryCacheEnabled bool
 
 var LogConsumeEnabled = true
+var RequestAuditEnabled = false
 
 var TLSInsecureSkipVerify bool
 var InsecureTLSConfig = &tls.Config{InsecureSkipVerify: true}
